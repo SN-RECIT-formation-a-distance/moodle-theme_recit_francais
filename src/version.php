a352e87c9178a5dc344d8c2279942194d61d3476
Yvon Quemener <yvon.quemener@csdps.qc.ca>
2020-04-02 13:43:37 -0400

    r6

--- src/version.php
+++ src/version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();                                                                                                
  
 // This is the version of the plugin.                                                                                               
-$plugin->version = '2020022900';                                                                                                    
+$plugin->version = 2020040100;                                                                                                    
 
 // This is the version of Moodle this plugin requires.                                                                              
 $plugin->requires = 2018050800;                                                                                                   
@@ -36,9 +36,9 @@ $plugin->requires = 2018050800;
 $plugin->component = 'theme_recit_francais';   
 
 // This is the named version.
-$plugin->release = 'R5-2020032700';                                                                                              
+$plugin->release = 'R6-2020040100';                                                                                              
  
 // This is a list of plugins, this plugin depends on (and their versions).                                                          
 $plugin->dependencies = [                                                                                                           
-    'theme_recit' => '2020022900'                                                                                                   
+    'theme_recit' => 2020040100                                                                                                   
 ];
