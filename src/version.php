d8b70594a6674587c5dd3a24c66b7626ffc89d94
Yvon Quemener <yvon.quemener@csdps.qc.ca>
2020-09-18 10:23:20 -0400

    patch r10

--- src/version.php
+++ src/version.php
@@ -36,7 +36,7 @@ $plugin->requires = 2018050800;
 $plugin->component = 'theme_recit_francais';   
 
 // This is the named version.
-$plugin->release = 'R10-V1.9.2';                                                                                              
+$plugin->release = 'R10-V1.10.2';                                                                                              
  
 // This is a list of plugins, this plugin depends on (and their versions).                                                          
 $plugin->dependencies = [                                                                                                           
